3f903bdecc48384c28e27f722efeb46611582090
seanwascoding <a225108347171@gmail.com>
2023-12-16 14:36:16 +0800

    version-1

---
+++ DrawingForm/DoubleBufferedPanel.cs
@@ -0,0 +1,11 @@
+﻿using System.Windows.Forms;
+namespace DrawingForm
+{
+    class DoubleBufferedPanel : Panel
+    {
+        public DoubleBufferedPanel()
+        {
+            DoubleBuffered = true;
+        }
+    }
+}
